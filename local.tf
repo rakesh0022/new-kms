73d4eb4442796872a5bcd3722652d24ccfdc8b25
rakesh0022 <92107358+rakesh0022@users.noreply.github.com>
2022-05-07 02:48:28 +0530

    Update local.tf

--- local.tf
+++ local.tf
@@ -3,12 +3,13 @@ data "google_project" "project" {
   
 }
 
+/*
 locals {
   
   purpose = var.purpose != "" ? var.purpose : ENCRYPT_DECRYPT
   rotation_period = local.purpose == "ENCRYPT_DECRYPT" ? var.rotation_period : null
 
-
+*/
 iam_additive_pairs = flatten ([
     for role in keys(var.iam_role_members) : [
      for member in lookup(var.iam_role_members, role, []) :
