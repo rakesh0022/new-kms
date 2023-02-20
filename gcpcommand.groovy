cdd39337bb333018df51e0f42c5f8965f026e1a4
engineer <92107358+rakesh0022@users.noreply.github.com>
2023-02-21 00:59:10 +0530

    created jenkinsfile

---
+++ gcpcommand.groovy
@@ -0,0 +1,26 @@
+pipeline {
+  agent any
+  
+  environment {
+    PROJECT_ID = "qwiklabs-gcp-00-e068f879d692"
+    SERVICE_ACCOUNT_CREDENTIALS = credentials('google-sa')
+  }
+  
+  parameters {
+    string(name: 'GCP_COMMAND', defaultValue: '', description: 'Enter the GCP command to execute')
+    string(name: 'GCP_COMMAND_ARGS', defaultValue: '', description: 'Enter the arguments for the GCP command')
+  }
+  
+  stages {
+    stage('Execute GCP Command') {
+      steps {
+        withCredentials([string(credentialsId: 'your-credentials-id', variable: 'GCP_CREDS')]) {
+          withEnv(["GOOGLE_APPLICATION_CREDENTIALS=${GCP_CREDS}"]) {
+            sh "gcloud config set project ${env.PROJECT_ID}"
+            sh "${params.GCP_COMMAND} ${params.GCP_COMMAND_ARGS}"
+          }
+        }
+      }
+    }
+  }
+}
